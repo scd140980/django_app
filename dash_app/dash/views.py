f8b8f54a67fd10617fdf48808386cd10b4b28615
scd140980 <semeniuccatalin@gmail.com>
2022-07-22 18:02:22 +0300

    feat: update app

--- dash_app/dash/views.py
+++ dash_app/dash/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+
+posts = [
+    {
+        'author': 'scd',
+        'title': 'dash post 1',
+        'content': 'First post content',
+        'date_posted': 'jul 22, 2022'
+    },
+    {
+        'author': 'scd1',
+        'title': 'dash post 2',
+        'content': 'Sec post content',
+        'date_posted': 'jul 23, 2022'
+    },
+]
+
 # Create your views here.
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'dash/home.html', context)
+
+def services(request):
+    return render(request, 'dash/services.html', {'title':'Services'})
